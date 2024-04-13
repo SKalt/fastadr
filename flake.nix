1ff98e9381de5d380aae7040b4cab91e3f1a3949
Steven Kalt <kalt.steven@gmail.com>
2024-04-13 11:26:21 -0400

    feat: draft auth provider for local development
    this includes:
    - adding a Nix flake
    - adding a (podman) Compose file

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = ""; # FIXME: add a description
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; # TODO: pin
+    flake-utils.url = "github:numtide/flake-utils"; # TODO: pin
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+        # Generate a user-friendly version number.
+        version = builtins.substring 0 8 self.lastModifiedDate;
+        info = (builtins.fromTOML (builtins.readFile ./pyproject.toml));
+        filters = import ./nix/filter_filesets.nix { inherit (pkgs) lib; };
+      in
+      {
+        # For `nix develop`:
+        devShell = pkgs.mkShell {
+          # see https://github.com/NixOS/nixpkgs/issues/52447
+          # see https://hoverbear.org/blog/rust-bindgen-in-nix/
+          # see https://slightknack.dev/blog/nix-os-bindgen/
+          # https://nixos.wiki/wiki/Rust#Installation_via_rustup
+          buildInputs = with pkgs;
+            [
+              # python tools
+              python311
+              poetry
+              ruff-lsp
+
+              # nix support
+              nixpkgs-fmt
+              nil
+
+              # virtualization
+              podman
+              podman-compose
+
+              # other
+              bashInteractive
+              git
+              gitleaks
+              lychee
+              shellcheck
+            ] ++ (with pkgs.python311Packages; [ pip ]);
+
+          # Environment variables
+        };
+      }
+    );
+}
